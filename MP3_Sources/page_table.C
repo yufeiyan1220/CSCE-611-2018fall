0ff8857e8eac38633e7486bb60260c7b317f5695
yufeiyan1220 <yufeiyan1220@live.com>
2018-09-27 17:14:17 -0500

    mp3

---
+++ MP3_Sources/page_table.C
@@ -0,0 +1,109 @@
+#include "assert.H"
+#include "exceptions.H"
+#include "console.H"
+#include "paging_low.H"
+#include "page_table.H"
+
+PageTable * PageTable::current_page_table = NULL;
+unsigned int PageTable::paging_enabled = 0;
+ContFramePool * PageTable::kernel_mem_pool = NULL;
+ContFramePool * PageTable::process_mem_pool = NULL;
+unsigned long PageTable::shared_size = 0;
+
+
+
+void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
+                            ContFramePool * _process_mem_pool,
+                            const unsigned long _shared_size)
+{
+   //assert(false);
+   kernel_mem_pool = _kernel_mem_pool;
+   process_mem_pool = _process_mem_pool;
+   shared_size = _shared_size;
+   Console::puts("Initialized Paging System\n");
+   
+}
+
+PageTable::PageTable()
+{
+	
+	//get 1 frame(4kb) to store page directory
+	unsigned long page_directory_frame = (*kernel_mem_pool).get_frames(1);
+	(*this).page_directory = (unsigned long*) (page_directory_frame * PAGE_SIZE);
+	
+	
+	//get another 1 frame next, 4kb for page table page
+	unsigned long getframe_address = kernel_mem_pool->get_frames(1) *PAGE_SIZE;
+	//The first directory marked as su, read&write, present.
+	((*this).page_directory)[0] = (unsigned long)(getframe_address | 3);
+	//page_table = first page table page 
+	unsigned long *page_table = (unsigned long*) (page_directory[0] & 0xFFFFF000);
+	
+	//set up a page table
+	// map the first 4MB of memory
+	unsigned long address=0; 
+	for(int i = 0; i < 1024; i++)
+	{
+		page_table[i] = address | 3; 
+		address = address + 4096; 
+	};
+	
+	//(*this).page_directory[0] |= 0x3;
+	for(int i = 1; i < 1024; i++)
+	{
+		page_directory[i] = 0 | 2; 
+	// attribute set to: supervisor level, read/write, not present(010 in binary)
+	};
+	
+	Console::puts("Constructed Page Table object\n");
+}
+
+
+void PageTable::load()
+{
+	current_page_table = this;
+	write_cr3((unsigned long)(*current_page_table).page_directory);
+	
+	Console::puts("Loaded page table\n");
+}
+
+void PageTable::enable_paging()
+{
+	//assert(false);
+	paging_enabled = 1;
+	write_cr0(read_cr0() | 0x80000000);
+	Console::puts("Enabled paging\n");
+}
+
+void PageTable::handle_fault(REGS * _r)
+{
+	unsigned int error_no = _r-> int_no;
+	//assert(false);
+	//page fault signal 14
+	if(error_no == 14){
+		//read the cr3 to get the directory frame num
+		unsigned long *current_page_directory = (unsigned long*) read_cr3();
+		unsigned long *page_table;
+		unsigned long address = read_cr2();
+		//get the first 10 bits for directory
+		unsigned long dir_index = address >> 22;
+		//if the requested page table is not valid
+		if((current_page_directory[dir_index] & 1) != 1){	
+			//get a frame for the requested dir
+			current_page_directory[dir_index] = (unsigned long)(kernel_mem_pool->get_frames(1)*PAGE_SIZE);
+			current_page_directory[dir_index] |= 3;
+			
+			page_table = (unsigned long*) (current_page_directory[dir_index] & 0xFFFFF000);
+			for(int i = 0; i < 1024; i++){
+				page_table[i] = 0 | 2;
+			}
+		}
+		//get the next 10 bit for page table page
+		unsigned long table_index = ((address>>12) & 0x3FF);
+		page_table = (unsigned long*)(current_page_directory[dir_index] & 0xFFFFF000);
+		page_table[table_index] = (unsigned long)(process_mem_pool->get_frames(1)*PAGE_SIZE) | 3;
+		
+	}
+	Console::puts("handled page fault\n");
+}
+
